db796907c2721f77fd4f39cea697468106c4ee4a
Brian K Preston <brian8181@gmail.com>
2021-10-23 06:11:37 -0500

    commit many changes

---
+++ src/pfp.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+// #include <regex>
+// #include <getopt.h>
+// #include "main.hpp"
+#include "pfp.hpp"
+
+Token::Token(string& value)
+{
+
+}
+
+TerminalNode::TerminalNode(Token* token)
+{
+
+}
+
+
+TerminalNode::TerminalNode(string& token)
+{
+    
+}
+
+// BinaryNode::BinaryNode(Token* token, TerminalNode* left, TerminalNode* right)
+// {
+
+// }
+
---
+++ src/pfp.hpp
@@ -0,0 +1,92 @@
+#ifndef _pfp_HPP
+#define _pfp_HPP
+
+#include <iostream>
+#include <string>
+
+using std::string;
+
+// constants
+
+// Options Flags
+unsigned char DEFAULTS = 0;
+unsigned char option_flags = 0; 
+
+enum TokenType
+{
+    Number,
+    Operator
+};
+
+class Token
+{
+
+public:
+
+    Token(string& value);
+
+private:
+
+    	const static int last_id = 0;
+    	int id;
+	TokenType type = TokenType::Number;
+	string value;
+	
+};
+
+class Node
+{
+
+public:
+
+    Node* GetParent();
+    void SetParent(Node& parent);
+
+private:
+
+    Node* parent = 0;
+
+};
+
+class TerminalNode
+{
+
+public:
+
+    TerminalNode(string& token);
+    TerminalNode(Token* token);
+
+    Token* GetToken();
+    void SetToken(Token& token);
+
+private:
+
+    Token* token = 0;
+
+};
+
+
+class BinaryNode : public TerminalNode
+{
+
+public:
+
+    BinaryNode(Token* token, TerminalNode* left, TerminalNode* right);
+
+private:
+
+    TerminalNode* left;
+    TerminalNode* right;
+
+};
+
+class PostfixParser
+{
+
+public:
+
+private:
+
+};
+
+#endif
