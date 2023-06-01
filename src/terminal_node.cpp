8db538cada45a7f7da4a3c212a1673468687f951
Brian K Preston <brian8181@gmail.com>
2023-06-01 10:22:33 -0500

    update all ...

--- src/terminal_node.cpp
+++ src/terminal_node.cpp
@@ -1,1 +1,17 @@
 #include "terminal_node.hpp"
+//#include "token.hpp"
+
+terminal_node::terminal_node(string& _token)
+{
+
+}
+
+// terminal_node::terminal_node(token* _token)
+// {
+
+// }
+
+// void terminal_node::set_token(token* _token)
+// {
+
+// }
--- src/terminal_node.hpp
+++ src/terminal_node.hpp
@@ -10,7 +10,7 @@ class terminal_node: public node
 public:
 
     // terminal_node();
-    // terminal_node(string& _token);
+    terminal_node(string& _token);
     // terminal_node(token* _token);
     // void set_token(token* _token);
 
