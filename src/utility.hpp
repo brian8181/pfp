87b3128b2f881c7ce7878c828ea283622a7750ff
Brian K Preston <brian8181@gmail.com>
2023-06-14 19:11:30 -0500

    add include guard

--- src/utility.hpp
+++ src/utility.hpp
@@ -1,6 +1,11 @@
+#ifndef _utility_HPP
+#define _utility_HPP
+
 #include <iostream>
 #include <algorithm>
 
 /*Simple Is-Numeric function in C++*/
 bool is_digit(const char value) { return std::isdigit(value); }
 bool is_numeric(const std::string& value) { return std::all_of(value.begin(), value.end(), is_digit); }
+
+#endif
