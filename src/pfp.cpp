ea19eddcd09794f29fa8df92967ffc776b4df046
Brian K Preston <brian8181@gmail.com>
2023-06-16 09:14:10 -0500

    continue port ...

--- src/pfp.cpp
+++ src/pfp.cpp
@@ -68,7 +68,7 @@ int parse_options(int argc, char* argv[])
     cout << "Initializing ..." << endl;
     cout << "id = " << t1.get_id() << endl;
     cout << "id = " << t2.get_id() << endl; 
-    // cout << "is_number(2) = " << is_integer('2') << endl;
+    // cout << "is_integer("2") = " << is_integer("2") << endl;
     // cout << "is_digit(2) = " << is_digit('2') << endl;
     // cout << "is_digit(a) = " << is_digit('a') << endl;
     // cout << "is_digit(.) = " << is_digit('.') << endl;
