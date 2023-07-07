0ef57685d7d4478099ce8e3aa35abd0fffcd4e56
Brian K Preston <brian8181@gmail.com>
2023-07-07 11:13:42 -0500

    add id function node...

--- src/node.hpp
+++ src/node.hpp
@@ -9,13 +9,19 @@ public:
 
     node(const node& n);
     node(const std::string& name);
+    int get_id();
     node* get_parent();
     void set_parent(node* parent);
-
+    
 protected:
 
     std::string _name;
     node* _parent;
+
+private:
+
+    int _id;
+    static int _prev_id;
 };
 
 #endif
--- src/token.cpp
+++ src/token.cpp
@@ -1,13 +1,13 @@
 #include "token.hpp"
 #include "utility.hpp"
 
-int token::_prev_id = 1;
+int token::_prev_id = 0;
 
 token::token(string value) : _type(token_type::Operator)
 {
+    _id = ++_prev_id;
     _value = value;
-    _id = _prev_id++;
-    
+        
     if(is_numeric(value))
         _type = token_type::Number;
     else
@@ -16,8 +16,8 @@ token::token(string value) : _type(token_type::Operator)
 
 token::token(const token& tok)
 {
-    _value = tok._value;
     _id = tok._id;
+    _value = tok._value;
     _type = tok._type;
 }
 
