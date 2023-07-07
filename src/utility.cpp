03ba7d873b40d2fb9ac9d3f0e69edfd2c33cf1d7
Brian K Preston <brian8181@gmail.com>
2023-07-07 17:50:39 -0500

    formatting changes ...

--- src/utility.cpp
+++ src/utility.cpp
@@ -25,8 +25,6 @@ bool is_digit(const char value)
 
 bool is_numeric(const std::string& s) 
 { 
-    //return std::all_of(value.begin(), value.end(), is_digit); 
-
     std::string::const_iterator end =  s.end();
     for(std::string::const_iterator iter = s.begin(); iter != end; ++iter)
     {
@@ -38,23 +36,23 @@ bool is_numeric(const std::string& s)
 
 std::string &ltrim(std::string &s)
 {
-    auto it = std::find_if(s.begin(), s.end(),
-                           [](char c)
-                           {
-                               return !std::isspace<char>(c, std::locale::classic());
-                           });
-    s.erase(s.begin(), it);
+    auto iter = std::find_if( s.begin(), s.end(), 
+        [](char c)
+        {
+            return !std::isspace<char>(c, std::locale::classic());
+        } );
+    s.erase(s.begin(), iter;
     return s;
 }
 
 std::string &rtrim(std::string &s)
 {
-    auto it = std::find_if(s.rbegin(), s.rend(),
-                           [](char c)
-                           {
-                               return !std::isspace<char>(c, std::locale::classic());
-                           });
-    s.erase(it.base(), s.end());
+    auto iter = std::find_if( s.rbegin(), s.rend(), 
+        [](char c)
+        {
+            return !std::isspace<char>(c, std::locale::classic());
+        } );
+    s.erase(iter.base(), s.end());
     return s;
 }
 
