ac2e0deb2201b1dba7211e2f678f561844f580cd
Brian K Preston <brian8181@gmail.com>
2023-07-11 16:13:55 -0500

    add main_test.cpp

---
+++ src/main_test.cpp
@@ -0,0 +1,22 @@
+// License:    GPL
+// Author:     Brian K Preston
+// File Name:  main.cpp
+// Build Date: Mon May 29 09:32:07 AM CDT 2023
+// Version:    0.0.1
+
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <iostream>
+#include "token.hpp"
+#include "node.hpp"
+#include "terminal_node.hpp"
+#include "binary_node.hpp"
+
+int main(int argc, char* argv[])
+{
+	//token t1;
+    //node n;
+    
+    return 0;
+}
