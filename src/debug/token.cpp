12206e99d3cbf285fa9f16f3934937fc1dfcda1d
Brian K Preston <brian8181@gmail.com>
2023-07-11 16:40:07 -0500

    add debug folder/files

---
+++ src/debug/token.cpp
@@ -0,0 +1,48 @@
+// License:    None
+// Author:     Brian K Preston
+// File Name:  token.cpp
+// Build Date: Sun Jul  9 09:09:59 AM CDT 2023
+// Version:    0.0.1
+
+#include "token.hpp"
+#include "utility.hpp"
+
+int token::_prev_id = 0;
+
+// token::token()
+// {
+    
+// }
+
+token::token(string value) : _type(token_type::Operator)
+{
+    _id = ++_prev_id;
+    _value = value;
+        
+    if(is_numeric(value))
+        _type = token_type::Number;
+    else
+        _type = token_type::Operator;
+}
+
+token::token(const token& t)
+{
+    _id = t._id;
+    _value = t._value;
+    _type = t._type;
+}
+
+int token::get_id()
+{
+    return _id;
+}
+
+token_type token::get_type()
+{
+    return _type;
+}
+
+string token::get_value()
+{
+    return _value;
+}
---
+++ src/debug/token.hpp
@@ -0,0 +1,40 @@
+// License:    None
+// Author:     Brian K Preston
+// File Name:  token.cpp
+// Build Date: Sun Jul  9 09:09:59 AM CDT 2023
+// Version:    0.0.1
+
+#ifndef _token_HPP
+#define _token_HPP
+
+#include <string>
+
+using std::string;
+
+enum token_type
+{
+    Number = 0x0,
+    Operator = 0x2
+};
+
+class token
+{
+public:
+
+    token() {};
+    token(string value);
+    token(const token& t);
+    
+    int get_id();
+    token_type get_type();
+    string get_value();
+   
+private:
+
+    int _id;
+    token_type _type;
+    string _value;
+    static int _prev_id;
+};
+
+#endif
